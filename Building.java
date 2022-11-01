fbfdcc9721fc41bd6f1be38704b0c02703a265f8
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-01 09:14:05 -0400

    Update Building.java

--- Building.java
+++ Building.java
@@ -10,10 +10,14 @@ public class Building {
 		g.fillRect(5+10, Math.round(640/3) + 10, 30, 30);
 		
 		for (int i=15; i < 1020/5; i+=40) {
-			
-			for (int j=Math.round(640/3); j < Math.round(640*2/3); j+=40) {
-				g.fillRect(15, j + 10, 30, 30);
-				g.fillRect(i, Math.round(640/3) + 10, 30, 30);
+			for (int j=Math.round(640/3); j < Math.round(640*5/6); j+=40) {
+				int n = (int) (Math.random()*2);
+				if (n == 1) g.setColor(Color.YELLOW);
+				g.fillRect(i, j + 10, 30, 30);
+				g.setColor(Color.BLACK);
+				int x = (int) (Math.random()*2);
+				if (x == 1) g.setColor(Color.YELLOW);
+				g.fillRect(i, j + 10, 30, 30);
 			}
 		}
 		
