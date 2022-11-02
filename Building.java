b7e57bc7b03cf5852e2382b62e719c4530486e90
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-02 09:13:58 -0400

    Update Building.java

--- Building.java
+++ Building.java
@@ -1,16 +1,39 @@
 import java.awt.*;
 
 public class Building {
+	private int x,y,w,h;
+	
+	public Building (int x, int y, int w, int h) {
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	public int getW() {
+		return w;
+	}
+	public int getH() {
+		return h;
+	}
+	
 	public void paint(Graphics2D g) {
 		//building
 		g.setColor(Color.GRAY);
-		g.fillRect(5, Math.round(640/3), (1020/5), Math.round(640*2/3));
+		g.fillRect(x, y, w, h);
 		
 		g.setColor(Color.BLACK);
-		g.fillRect(5+10, Math.round(640/3) + 10, 30, 30);
+		//g.fillRect(x+10, y + 10, 30, 30);
 		
-		for (int i=15; i < 1020/5; i+=40) {
-			for (int j=Math.round(640/3); j < Math.round(640*5/6); j+=40) {
+		for (int i=x+10; i+30 < w+getX(); i+=40) {
+			for (int j=y; j+30 < h+getY()*2/3; j+=40) {
 				int n = (int) (Math.random()*2);
 				if (n == 1) g.setColor(Color.YELLOW);
 				g.fillRect(i, j + 10, 30, 30);
