590311a422f01bf6406769a951996394416d6726
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-03 07:59:28 -0400

    Update Cityscape.java

--- Cityscape/Cityscape.java
+++ Cityscape/Cityscape.java
@@ -5,7 +5,13 @@ public class Cityscape extends JPanel {
 
 	//private Cityscape cs = new Cityscape();
 	private Building b = new Building(5, Math.round(640/3), 1020/5, Math.round(640/3)*2);
-
+	private Building b = new Building(5, Math.round(640/3), (1020/5), Math.round(640*2/3));
+	private Building b1 = new Building(15 + b.getW(), Math.round(640/3), Math.round(1020/8), Math.round(640*2/3));
+	private Building b2 = new Building(15 + b1.getX() + b1.getW(), Math.round(640/3), Math.round(1020/4), Math.round(640*2/3));
+	private Building b3 = new Building(15 + b2.getX() + b2.getW(), Math.round(640/3), Math.round(1020/10), Math.round(640*2/3));
+	private Building b4 = new Building(15 + b3.getX() + b3.getW(), Math.round(640/3), Math.round(1020/7), Math.round(640*2/3));
+	private Building b5 = new Building(15 + b4.getX() + b4.getW(), Math.round(640/3), Math.round(1020/12), Math.round(640*2/3));
+	
 	public UFO u = new UFO(this); // Add an instance of the ball object
 
 	public void move() {
@@ -22,7 +28,12 @@ public class Cityscape extends JPanel {
 
 		//cs.paint(g2d);
 		b.paint(g2d);
-
+		b1.paint(g2d);
+		b2.paint(g2d);
+		b3.paint(g2d);
+		b4.paint(g2d);
+		b5.paint(g2d);
+		
 		// Smooth’s out the movement
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		u.paint(g2d);
