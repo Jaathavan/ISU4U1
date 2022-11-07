db85e0360bcd1b014c4d3e41d05f6a22f6bb2806
Jaathavan <rjaathu@outlook.com>
2022-11-07 09:47:17 -0500

    ball

--- Ball/Ball.java
+++ Ball/Ball.java
@@ -1,35 +1,78 @@
-package BallAnimation;
-
 import java.awt.*;
-import javax.swing.*;
+import java.awt.event.*;
 
 public class Ball {
-	private int x = 20;
-	private int y = 20;
-	private int xa = 1; // Horizontal speed
-	private int ya = 1; // Vertical speed
-	private BallAnimation ba;
+	private int x = 0;
+	private int y = 0;
+	private int xa = 0;
+	private int ya = 0;
+	// These are used to keep track of how the ball is moving, by doing it this
+	// way the animation is smoother
+	private boolean right = false, left = false;
+	private Boolean up = false, down = false;
 
-	public Ball(BallAnimation ba) {
-		this.ba = ba;
+	// Because this is not an actual Key Listener, you don’t have to have all
+	// three methods, you can for encapsulation reasons only include the one’s
+	// you need.
+	public void keyPressed(KeyEvent e) {
+		// This checks to see which key was pressed, and then sets the appropriate
+		// Boolean to true
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			up = true;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			down = true;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		// When the key is released, set the Boolean to false, and change
+		// acceleration to 0
+		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = false;
+			xa = 0;
+		}
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = false;
+			xa = 0;
+		}
+		
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			up = false;
+			ya = 0;
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			down = false;
+			ya = 0;
+		}
 	}
 
 	public void move() {
-		if (x + xa < 0) // If the ball exceeds the left
-			xa = 1; // go right
-		if (x + xa > ba.getWidth() - 50) // If the ball exceeds the right
-			xa = -1; // go left
-		if (y + ya < 0) // If the ball exceeds the top
-			ya = 1; // go down
-		if (y + ya > ba.getHeight() - 50) // If the ball exceeds the bottom
-			ya = -1; // go up
-			x = x + xa; // Adjust the x and y coordinates
-			y = y + ya; // by the appropriate speeds
-
+		// Base your acceleration on the Booleans set by your keyPressed method
+		if (right) {
+			xa = 1;
+		}
+		if (left) {
+			xa = -1;
+		}
+		if (down) {
+			ya = 1;
+		}
+		if (up) {
+			ya = -1;
+		}
+		x += xa;
+		y += ya;
 	}
 
 	public void paint(Graphics2D g2d) {
-		g2d.setColor(Color.BLACK);
 		g2d.fillOval(x, y, 50, 50);
 	}
 }
--- Ball/BallAnimation.java
+++ Ball/BallAnimation.java
@@ -1,36 +1,61 @@
-package BallAnimation;
-
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.*;
 
 public class BallAnimation extends JPanel {
+	Ball b = new Ball();
 
-	private Ball b = new Ball(this); // Add an instance of the ball object
+	public BallAnimation() {
+		// This adds the KeyListener to the BallAnimation. It’s in this section that
+		// you HAVE to have all three key listener methods whether they are used
+		// or not. We place the KeyListener in the JPanel class because as we
+		// mentioned before it is the director of all the action, so it receives
+		// the keyboard information and then passes it along to the appropriate
+		// classes.
+		addKeyListener(new KeyListener() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+			}
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				// Passes the KeyEvent e to the ball instance
+				b.keyReleased(e);
+			}
+
+			@Override
+			public void keyPressed(KeyEvent e) {
+				// Passes the KeyEvent e to the ball instance
+				b.keyPressed(e);
+			}
+		}); // NOTE THE SEMI-COLON!!!!
+		// Making sure our JPanel has the focus and therefor it is the
+		// instance that will receive the keyboard input
+		setFocusable(true);
+	}
 
 	private void move() {
 		b.move();
 	}
 
+	@Override
 	public void paint(Graphics g) {
-		super.paint(g); // Clears the panel, for a fresh start
+		super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
-		// Smooth’s out the movement
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		b.paint(g2d);
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		JFrame f = new JFrame("Ball Animation 1");
-		f.setSize(300, 300);
-		f.setVisible(true);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		BallAnimation p = new BallAnimation();
-		f.add(p);
+		JFrame frame = new JFrame("Ball World");
+		BallAnimation bw = new BallAnimation();
+		frame.add(bw);
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		while (true) {
-			p.move(); // Updates the coordinates
-			p.repaint(); // Calls the paint method
-			Thread.sleep(10); // Pauses for a moment
+			bw.move();
+			bw.repaint();
+			Thread.sleep(10);
 		}
 	}
-
 }
