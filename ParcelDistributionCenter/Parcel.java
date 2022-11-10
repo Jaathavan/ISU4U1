054e0496931d724463cfe3b7d8fed1e2e33e6922
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-10 10:30:46 -0500

    Update Parcel.java

--- ParcelDistributionCenter/Parcel.java
+++ ParcelDistributionCenter/Parcel.java
@@ -1,14 +1,17 @@
-public class Parcel {
-    private int l,w,h,c;
-    private int x,y;
+import java.awt.*;
 
-    public Parcel (int l, int w, int h, int c) {
+public class Parcel {
+    private int l,h, w, c;
+    private int x,y,wxy;
+
+    public Parcel (int l, int h, int w, int c) {
         this.l = l;
-        this.w = w;
         this.h = h;
+        this.w = w;
         this.c = c;
         x = 0;
         y= 300;
+        wxy = this.w*(int)Math.sqrt(2)/2;
     }
 
     public int getX() {
@@ -18,10 +21,47 @@ public class Parcel {
     public int getY() {
         return y;
     }
+    
+    public int getL() {
+        return l;
+    }
+    
+    public int getH() {
+        return h;
+    }
+    
+    public int getW() {
+        return w;
+    }
+    
+    public int getC() {
+        return c;
+    }
+    
+    public int getWXY() {
+        return wxy;
+    }
 
     public void move() {
         x++;
     }
-
+    
+    public void paint(Graphics2D g) {
+    	if (getC() == 0) g.setColor(Color.BLUE);
+    	if (getC() == 1) g.setColor(Color.GREEN);
+    	if (getC() == 2) g.setColor(Color.YELLOW);
+    	
+        g.fillPolygon(new int[] {getX(), getX()+getL(), getX()+getL()+getWXY(), getX()+getL()+getWXY(), getX()+getWXY(), getX()}, new int[] {getY(), getY(), getY()+getWXY(), getY()+getWXY()+getH(), getY()+getWXY()+getH(), getY()+getH()}, 6);
+        g.setColor(Color.BLACK);
+        g.drawLine(getX(), getY(), getX()+getL(), getY());
+        g.drawLine(getX()+getL(), getY(), getX()+getL()+getWXY(), getY()+getWXY());
+        g.drawLine(getX()+getL()+getWXY(), getY()+getWXY(), getX()+getL()+getWXY(), getY()+getWXY()+getH());
+        g.drawLine(getX()+getL()+getWXY(), getY()+getWXY()+getH(), getX()+getWXY(), getY()+getWXY()+getH());
+        g.drawLine(getX()+getWXY(), getY()+getWXY()+getH(), getX(), getY()+getH());
+        g.drawLine(getX(), getY()+getH(), getX(), getY());
+        g.drawLine(getX()+getWXY(), getY()+getWXY()+getH(), getX()+getWXY(), getY()+getWXY());
+        g.drawLine(getX()+getWXY(), getY()+getWXY(), getX(), getY());
+        g.drawLine(getX()+getWXY(), getY()+getWXY(), getX()+getL()+getWXY(), getY()+getWXY());
+    }
     
 }
