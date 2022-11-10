dec22a2c21b6e1437c6ba0b72499cbb19bafe641
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-10 10:29:52 -0500

    Update ParcelDistributionCenter.java

--- ParcelDistributionCenter/ParcelDistributionCenter.java
+++ ParcelDistributionCenter/ParcelDistributionCenter.java
@@ -2,29 +2,40 @@ import java.awt.*;
 import javax.swing.*;
 
 public class ParcelDistributionCenter extends JPanel {
-    private int x= 100;
-    private int y = 50;
-    private int l = 100;
-    private int w = 100;
-    private int h = 100;
-    private int wxy = w*(int)Math.sqrt(2)/2;
-
+    public int randLH() {
+    	return (int)(Math.random()*30) +20;
+    }
+    
+    public int randW() {
+    	return (int)(Math.random()*40) +10;
+    }
+    
+    public int randC() {
+    	return (int) (Math.random()*3);
+    }
+    
+	
+	private Parcel p = new Parcel(randLH(), randLH(), randW(), randC());
+	
+	public void move() {
+		p.move();
+	}
+	
     @Override
     public void paint(Graphics g) {
-        super.paint(g);
+    	super.paint(g);
 		Graphics2D g2d = (Graphics2D) g;
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		
 		//bg colour of gray
 		g.setColor(Color.DARK_GRAY);
 		g.fillRect(0, 0, 1020, 640);
+        p.paint(g2d);
 
-        g.setColor(Color.ORANGE);
-        g.drawPolygon(new int[] {x, x+l, x+l+wxy, x+l+wxy, x+wxy, x}, new int[] {y, y, y+wxy, y+wxy+h, y+wxy+h, y+h}, 6);
     }
 
     public static void main(String[] args) throws InterruptedException {
-		JFrame frame = new JFrame("Cityscape");
+		JFrame frame = new JFrame("Parcel Distribution Center");
 		ParcelDistributionCenter pdc = new ParcelDistributionCenter();
 		frame.add(pdc);
 		frame.setSize(1020, 640);
@@ -33,7 +44,7 @@ public class ParcelDistributionCenter extends JPanel {
 		
 		
 		while (true) {
-			//pdc.move();
+			pdc.move();
 			pdc.repaint();
 			Thread.sleep(10);
 		}
