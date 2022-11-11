ae2971294dc61dae2094da61bd56eb3a26be9e89
Jaathavan Ranjanathan <81396102+Jaathavan@users.noreply.github.com>
2022-11-11 10:32:37 -0500

    Update Scanner.java

---
+++ ParcelDistributionCenter/Scanner.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Scanner {
+	private boolean scan;
+	
+	public Scanner() {
+		this.scan = false;
+	}
+	
+	public void paint(Graphics g) {
+		g.setColor(Color.GRAY);
+		
+	}
+}
