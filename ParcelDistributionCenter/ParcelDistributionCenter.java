c99879985bbb3fd2e2f0473e8a704ae51de0c30b
Jaathavan <rjaathu@outlook.com>
2022-11-12 10:47:23 -0500

    simplify

--- ParcelDistributionCenter/ParcelDistributionCenter.java
+++ ParcelDistributionCenter/ParcelDistributionCenter.java
@@ -14,60 +14,25 @@ public class ParcelDistributionCenter extends JPanel {
 		return (int) (Math.random() * 3);
 	}
 
-	private Parcel p = new Parcel(randLH(), randLH(), randW(), randC(), 0);
-	private Parcel p1 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p.getX() - 200 - (p.getX() + p.getL() + p.getWXY()));
-	private Parcel p2 = new Parcel(randLH(), randLH(), randW(), randC(), p1.getX() - 200 - (p1.getL() + p1.getWXY()));
-	private Parcel p3 = new Parcel(randLH(), randLH(), randW(), randC(), p2.getX() - 200 - (p2.getL() + p2.getWXY()));
-	private Parcel p4 = new Parcel(randLH(), randLH(), randW(), randC(), p3.getX() - 200 - (p3.getL() + p3.getWXY()));
-	private Parcel p5 = new Parcel(randLH(), randLH(), randW(), randC(), p4.getX() - 200 - (p4.getL() + p4.getWXY()));
-	private Parcel p6 = new Parcel(randLH(), randLH(), randW(), randC(), p5.getX() - 200 - (p5.getL() + p5.getWXY()));
-	private Parcel p7 = new Parcel(randLH(), randLH(), randW(), randC(), p6.getX() - 200 - (p6.getL() + p6.getWXY()));
-	private Parcel p8 = new Parcel(randLH(), randLH(), randW(), randC(), p7.getX() - 200 - (p7.getL() + p7.getWXY()));
-	private Parcel p9 = new Parcel(randLH(), randLH(), randW(), randC(), p8.getX() - 200 - (p8.getL() + p8.getWXY()));
-	private Parcel p10 = new Parcel(randLH(), randLH(), randW(), randC(), p9.getX() - 200 - (p.getL() + p9.getWXY()));
-	private Parcel p11 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p10.getX() - 200 - (p10.getL() + p10.getWXY()));
-	private Parcel p12 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p11.getX() - 200 - (p11.getL() + p11.getWXY()));
-	private Parcel p13 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p12.getX() - 200 - (p12.getL() + p12.getWXY()));
-	private Parcel p14 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p13.getX() - 200 - (p13.getL() + p13.getWXY()));
-	private Parcel p15 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p14.getX() - 200 - (p14.getL() + p14.getWXY()));
-	private Parcel p16 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p15.getX() - 200 - (p15.getL() + p15.getWXY()));
-	private Parcel p17 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p16.getX() - 200 - (p16.getL() + p16.getWXY()));
-	private Parcel p18 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p17.getX() - 200 - (p17.getL() + p17.getWXY()));
-	private Parcel p19 = new Parcel(randLH(), randLH(), randW(), randC(),
-			p18.getX() - 200 - (p18.getL() + p18.getWXY()));
-	
+	Parcel[] list = new Parcel[20];
+
 	private Scanner s = new Scanner();
-	
+
+	public ParcelDistributionCenter() {
+		for (int i = 0; i < list.length; i++) {
+			if (i==0) {
+				list[i] = new Parcel(randLH(), randLH(), randW(), randC(), 0);
+			}
+			else {
+				list[i] = new Parcel(randLH(), randLH(), randW(), randC(), list[i-1].getX() - 200 - (list[i-1].getL() + list[i-1].getWXY()));
+			}
+		}
+	}
+
 	public void move() {
-		p.move();
-		p1.move();
-		p2.move();
-		p3.move();
-		p4.move();
-		p5.move();
-		p6.move();
-		p7.move();
-		p8.move();
-		p9.move();
-		p10.move();
-		p11.move();
-		p12.move();
-		p13.move();
-		p14.move();
-		p15.move();
-		p16.move();
-		p17.move();
-		p18.move();
-		p19.move();
+		for (Parcel p : list) {
+			p.move();
+		}
 	}
 
 	@Override
@@ -82,27 +47,11 @@ public class ParcelDistributionCenter extends JPanel {
 		g.fillRect(0, 0, 1020, 640);
 
 		// parcels
-		p.paint(g2d);
-		p1.paint(g2d);
-		p2.paint(g2d);
-		p3.paint(g2d);
-		p4.paint(g2d);
-		p5.paint(g2d);
-		p6.paint(g2d);
-		p7.paint(g2d);
-		p8.paint(g2d);
-		p9.paint(g2d);
-		p10.paint(g2d);
-		p11.paint(g2d);
-		p12.paint(g2d);
-		p13.paint(g2d);
-		p14.paint(g2d);
-		p15.paint(g2d);
-		p16.paint(g2d);
-		p17.paint(g2d);
-		p18.paint(g2d);
-		p19.paint(g2d);
+		for (Parcel p : list) {
+			p.paint(g2d);
+		}
 		
+		//scanner
 		s.paint(g2d);
 	}
 
--- ParcelDistributionCenter/Scanner.java
+++ ParcelDistributionCenter/Scanner.java
@@ -3,13 +3,41 @@ import javax.swing.*;
 
 public class Scanner {
 	private boolean scan;
-	
+
 	public Scanner() {
 		this.scan = false;
 	}
+
+	private boolean getScan() {
+		return scan;
+	}
+
+	private void setScan(boolean scan) {
+		this.scan = scan;
+	}
 	
+	public void move(int x) {
+		if (x >= 200 && x <= 300) {
+			setScan(true);
+		}
+		else {
+			setScan(false);
+		}
+	}
+
 	public void paint(Graphics g) {
 		g.setColor(Color.GRAY);
-		
+		g.fillRect(200, 300-20, 100, 100);
+
+		if (getScan() == true) {
+			g.setColor(Color.YELLOW);
+		} else {
+			g.setColor(Color.RED);
+		}
+
+		g.fillOval(200+10, 300-20+10, 10, 10);
+
+		g.setColor(Color.BLACK);
+		g.drawOval(200+10, 300-20+10, 10, 10);
 	}
 }
