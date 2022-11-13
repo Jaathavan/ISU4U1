6e67cfd056aa0f1c27125365a269f10575294491
Jaathavan <rjaathu@outlook.com>
2022-11-13 12:59:22 -0500

    add encapsulations

--- ParcelDistributionCenter/ConveyorBelt.java
+++ ParcelDistributionCenter/ConveyorBelt.java
@@ -17,37 +17,60 @@ public class ConveyorBelt {
         return on;
     }
 
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+    
+    public int getL() {
+        return l;
+    }
+    
+    public int getH() {
+        return h;
+    }
+
+    public int getXX() {
+        return xx;
+    }
+
+    public void setXX(int xx) {
+        this.xx = xx;
+    }
+
     public void setOn(boolean on) {
         this.on = on;
     }
     
     public void move() {
-        if (xx == x+20) {
-            xx-=20;
+        if (getXX() == getX()+20) {
+            setXX(getXX() - 20);
         }
-        xx++;
+        setXX(getXX() + 1);
     }
 
     public void paint(Graphics2D g) {
         g.setColor(Color.GRAY.darker().darker().darker().darker());
-        g.fillRoundRect(x, y-50, l, h+40, 10, 10);
+        g.fillRoundRect(getX(), getY()-50, getL(), getH()+40, 10, 10);
         
         g.setColor(Color.GRAY.darker().darker().darker().darker());
-        g.fillRoundRect(x, y+2, l, h, 10, 10);
-        for (int i=x; i < x+l; i+=15) {
+        g.fillRoundRect(getX(), getY()+2, getL(), getH(), 10, 10);
+        for (int i=getX(); i < getX()+getL(); i+=15) {
             g.setColor(Color.GRAY);
-            g.fillOval(i, y+3, 10, 10);
+            g.fillOval(i, getY()+3, 10, 10);
             g.setColor(Color.BLACK);
-            g.drawOval(i, y+3, 10, 10);
-            g.drawLine(i, y+10, i+5, y+8);
+            g.drawOval(i, getY()+3, 10, 10);
+            g.drawLine(i, getY()+10, i+5, getY()+8);
         }
-        for (int i=xx; i < xx+l; i+=15) {
-            //g.drawLine(i, y+10, i+5, y+8);
-            g.drawLine(i, y-50, i+20, y+2);
+        for (int i=getXX()-15; i < getXX()+getL(); i+=15) {
+            g.drawLine(i, getY()-50, i+20, getY()+2);
         }
         
         g.setColor(Color.BLACK);
-        g.drawRoundRect(x, y-50, l, h+40, 10, 10);
-        g.drawRoundRect(x, y+2, l, h, 10, 10);
+        g.drawRoundRect(getX(), getY()-50, getL(), getH()+40, 10, 10);
+        g.drawRoundRect(getX(), getY()+2, getL(), getH(), 10, 10);
     }
 }
