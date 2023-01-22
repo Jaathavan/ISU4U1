8f3e7e99a9b370f5097277f9f6c721b26fbedc9f
Jaathavan <rjaathu@outlook.com>
2023-01-22 13:58:50 -0500

    bubble

---
+++ SortAnalysis/SortAnalysis.java
@@ -0,0 +1,94 @@
+public class SortAnalysis {
+    public static void main(String[] args) {
+        StopWatch sw = new StopWatch();
+
+        //create 4 2d arrays 
+        int[][] random;
+        int[][] nearlySorted;
+        int[][] reversed;
+        int[][] fewUniqueValues;
+        long ranTime = 0;
+        long nsTime = 0;
+        long revTime = 0;
+        long fuvTime = 0;
+        
+        for (int i=10; i < 100000; i*=10) {
+            //initialize with size 10 and each array size
+            random = new int[10][i]; 
+            nearlySorted = new int[10][i];
+            reversed = new int[10][i];
+            fewUniqueValues = new int[10][i];
+            
+            //fill in array
+            for (int j=0; j < 10; j++) {
+                random[j] = ArrayGen.randomizedArray(i);
+                nearlySorted[j] = ArrayGen.nearlySortedArray(i);
+                reversed[j] = ArrayGen.reversedArray(i);
+                fewUniqueValues[j] = ArrayGen.nearlySortedArray(i);
+            }
+            
+            System.out.println("Array Size: "  + i);
+            
+            System.out.println("Bubble sort: ");
+            System.out.println();
+            //bubble sort
+            for (int k=0; k < 10; k++) {
+                sw.start();
+                Sorts.bubbleSort(random[k]);
+                sw.stop();
+                System.out.println("Random " + (k+1) + ": " + sw.getElapsedTime());
+                ranTime += sw.getElapsedTime();
+
+                sw.start();
+                Sorts.bubbleSort(nearlySorted[k]);
+                sw.stop();
+                System.out.println("NearlySorted " + (k+1) + ": " + sw.getElapsedTime());
+                nsTime += sw.getElapsedTime();
+
+                sw.start();
+                Sorts.bubbleSort(reversed[k]);
+                sw.stop();
+                System.out.println("Reversed " + (k+1) + ": " + sw.getElapsedTime());
+                revTime += sw.getElapsedTime();
+
+                sw.start();
+                Sorts.bubbleSort(fewUniqueValues[k]);
+                sw.stop();
+                System.out.println("FewUniqueValues " + (k+1) + ": " + sw.getElapsedTime());
+                fuvTime += sw.getElapsedTime();
+
+                System.out.println();
+            }
+
+            System.out.println("Average Bubble Sort: ");
+            System.out.println("Average Time Random: " + ranTime/10);
+            System.out.println("Average Time NearlySorted: " + nsTime/10);
+            System.out.println("Average Time Reversed: " + revTime/10);
+            System.out.println("Average Time FewUniqueValues: " + fuvTime/10);
+            System.out.println();
+
+            //reset variables and arrays
+            ranTime = 0;
+            nsTime = 0;
+            revTime = 0;
+            fuvTime = 0;
+            for (int j=0; j < 10; j++) {
+                random[j] = ArrayGen.randomizedArray(i);
+                nearlySorted[j] = ArrayGen.nearlySortedArray(i);
+                reversed[j] = ArrayGen.reversedArray(i);
+                fewUniqueValues[j] = ArrayGen.nearlySortedArray(i);
+            }
+
+
+            //selection sort
+            
+
+            //insertion sort
+
+            //merge sort
+
+            //quick sort
+
+        }
+    }
+}
